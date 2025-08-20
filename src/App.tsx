b1472afb430a6aeaa822079ef4759f1f79c13b53
ykoshevenko <ykoshevenko@list.ru>
2025-08-20 07:22:06 +0300

    realize

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className='all'>
+      <h1 className='title'>DaVinci - лучший React шаблон</h1>
+
+      <div className="typing-animation">
+        <span className='typing-text'>Попробуй, и реши, на чьей стороне ты!</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(),],
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+  },
+
+  build: {
+    outDir: 'build',
+    minify: 'terser',
+    emptyOutDir: true,
+  }
+})
